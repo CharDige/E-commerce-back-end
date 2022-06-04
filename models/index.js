5e308ce903f6b2cfc2aab8a6a22df32b7946fa3c
Charlotte Dige <99524327+CharDige@users.noreply.github.com>
2022-06-04 16:07:32 +0930

    Updated product and tags names when through ProductTag

--- models/index.js
+++ models/index.js
@@ -21,7 +21,7 @@ Product.belongsToMany(Tag, {
     model: ProductTag,
     unique: false
   },
-  as: 'product_tags'
+  as: 'tags'
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
@@ -29,7 +29,7 @@ Tag.belongsToMany(Product, {
     model: ProductTag,
     unique: false
   },
-  as: 'tagged_products'
+  as: 'products'
 })
 
 module.exports = {
